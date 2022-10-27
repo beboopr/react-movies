675cd565d7a29392007afddc3a4e9848242bdb10
beboopr <linnets-router.09@icloud.com>
2022-10-27 15:21:26 -0400

    react-movies

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,11 @@
+import movies from "../movies";
+
+function MovieList() {
+    return (
+        <section>
+            <h2>Movies go here..</h2>
+        </section>
+    )
+}
+
+export default MovieList;
